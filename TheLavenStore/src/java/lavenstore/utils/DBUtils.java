0935c759d600a9b5902a9677e023e49f4609df09
lion3993vn <lion3993vn@gmail.com>
2024-02-02 23:23:33 +0700

    conf: setup project

---
+++ TheLavenStore/src/java/lavenstore/utils/DBUtils.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lavenstore.utils;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
+
+/**
+ *
+ * @author giadu
+ */
+public class DBUtils {
+    private static final String DB_NAME = "TheLavenStore_PRJ301";
+    private static final String DB_USER_NAME = "sa";
+    private static final String DB_PASSWORD = "12345";
+    
+    public static Connection getConnection() throws ClassNotFoundException, SQLException {
+        Connection conn = null;
+        Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+        String url = "jdbc:sqlserver://localhost:1433;databaseName="+DB_NAME;
+        conn = DriverManager.getConnection(url, DB_USER_NAME, DB_PASSWORD);
+        return conn;
+    }
+}
